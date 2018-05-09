e4e3f69cf00e11ff9dd71befe96828845c6bfdc8
Tiramisu 1993 <sabergeass@gmail.com>
2018-05-09 09:41:34 +0800

    address error

--- src/main/java/com/website/controller/IndexController.java
+++ src/main/java/com/website/controller/IndexController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class IndexController {
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String loginview(){
+
         return "loginview";
     }
 
